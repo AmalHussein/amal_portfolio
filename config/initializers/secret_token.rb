5dad722d54e38907eab0da0170fe62d1794d4da5
Amal Hussein <amalhussein08@gmail.com>
2013-12-09 12:01:00 -0500

    setup new rails app, with mysql database, and added .elasticbeanstalk to the .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmalPortfolio::Application.config.secret_key_base = '7bb9f2024248ebb6c3acf8b2158c637f5116727afd7a93e25ab4e36313525f1871f51d5664ab15679d80ff262d127329a140b7a1dd681c0798808c0d7759f70c'
